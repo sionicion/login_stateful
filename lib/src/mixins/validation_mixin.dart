630484f94c9b8d9fdef45d3b94d1023b1562f452
Tyler Swindell <electromagnetic.jolt@gmail.com>
2019-01-31 01:37:27 -0500

    Initial commit

---
+++ lib/src/mixins/validation_mixin.dart
@@ -0,0 +1,12 @@
+class ValidationMixin {
+  String validateEmail(value){
+        if (!value.contains('@')){
+          return 'Please enter a valid email';
+        }
+        return null;
+      }
+  String validatePassword(value){
+        if (value.length < 4) {return 'Password must be at least 4 characters';}
+        return null;
+      }
+}
